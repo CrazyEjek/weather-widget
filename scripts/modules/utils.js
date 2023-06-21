32ad076d8b341ad617965716a7e24d0f0788fb60
CrazyEjek <myejek@gmail.com>
2023-06-21 15:17:29 +0500

    1 lesson

---
+++ scripts/modules/utils.js
@@ -0,0 +1,48 @@
+export const getcurrentDataTime = () => {
+    const months = [
+        'янв',
+        'фер',
+        'мар',
+        'апр',
+        'май',
+        'июн',
+        'июл',
+        'авг',
+        'сен',
+        'окт',
+        'ноя',
+        'дек',
+    ];
+
+    const weekdays = [
+        'воскресенье',
+        'понедельник',
+        'вторник',
+        'среда',
+        'четверг',
+        'пятница',
+        'суббота',
+    ];
+
+     const date = new Date();
+     
+     const dayOfMonth = date.getDate();
+     const month = months [date.getMonth()];
+     const year = date.getFullYear();
+     const dayOfWeek = weekdays [date.getDay()];
+
+     let hours = date.getHours();
+     let minutes = date.getMinutes();
+
+     if (hours < 10) {
+        hours = `0${hours}`;
+     }
+
+     if (minutes < 10) {
+        minutes = `0${minutes}`;
+     }
+
+    
+     return {month, year, dayOfMonth, dayOfWeek, hours, minutes};
+    
+};
