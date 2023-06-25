e42e74b9dd26f88c618aee6a54af9e2476243880
CrazyEjek <myejek@gmail.com>
2023-06-26 02:41:55 +0500

    lesson 3 end

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,25 +1,33 @@
-import { fetchWeather } from './APIservice.js';
+import { fetchForecast, fetchWeather } from './APIservice.js';
 import {
     renderWidgetToday,
     renderWidgetOther,
     renderWidgetForecast,
+    showError,
 } from './render.js';
 
 export const startWidget = async () => {
+
+    const city = 'Пермь';
     const widget = document.createElement('div');
     widget.classList.add('widget');
 
-    const dataWeather = await fetchWeather('Пермь');
+    const dataWeather = await fetchWeather(city);
 
     if (dataWeather.success) {
         renderWidgetToday(widget, dataWeather.data);
         renderWidgetOther(widget, dataWeather.data);
+    } else {
+        showError(dataWeather.error);
+
     }
-
-    // } else (show)
-
-    
-    renderWidgetForecast(widget);
+        const dataForecast = await fetchForecast(city);
+        if (dataForecast.success) {
+            renderWidgetForecast(widget, dataForecast.data);
+        } else {
+            showError(dataForecast.error);
+        }
+           
 
     return widget;
 };
